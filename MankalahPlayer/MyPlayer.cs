fc5b548c7ae2b034c3bcd69cd9fc73a0a8589371
Jacob-Brink <jacobpeterbrink@gmail.com>
2019-12-08 15:35:45 -0500

    Final Version

--- MankalahPlayer/MyPlayer.cs
+++ MankalahPlayer/MyPlayer.cs
@@ -12,25 +12,36 @@ namespace Mankalah
 {
 
     // rename me
-    public class MyPlayer : Player // class must be public
+    public class LeBrink : Player // class must be public
     {
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
 
         private const int bottomGoal = 6;
         private const int topGoal = 13;
         private int maxTime;
         private int turnCount = 0;
         private CancellationTokenSource cancellationToken;
+        private int depthMax;
 
-
-
+        /* DataWrapper class
+         * container for best move, value pair and second best move, second best value pair
+         */
         public class DataWrapper
         {
-            private int move, value;
+            private int move, value, secondBestMove, secondBestValue;
 
-            public DataWrapper(int move, int value)
+            public DataWrapper(int move, int value, int secondBestMove, int secondBestValue)
             {
                 this.move = move;
                 this.value = value;
+                this.secondBestMove = secondBestMove;
+                this.secondBestValue = secondBestValue;
             }
 
             public int getMove()
@@ -43,63 +54,122 @@ namespace Mankalah
                 return this.value;
             }
 
-        }
-
-        public class WeightsPackage
-        {
-            public double weight1 { get; }
-            public double weight2 { get; }
-            public double weight3 { get; }
-            public double weight4 { get; }
-
-            public WeightsPackage(double w1, double w2, double w3, double w4)
+            public int getSecondBestMove()
             {
-                weight1 = w1;
-                weight2 = w2;
-                weight3 = w3;
-                weight4 = w4;
+                return this.secondBestMove;
+            }
+
+            public int getSecondBestValue()
+            {
+                return this.secondBestValue;
             }
 
         }
 
         private Position position;
-        private WeightsPackage weightsPackage;
 
+        /* MyPlayer constructor
+         * 
+         */
         public MyPlayer(Position pos, int maxTimePerMove) // constructor must match this signature
-            : base(pos, "Pruning With Dynamic Row Strategy Based on Turn Count > 10", maxTimePerMove) // choose a string other than "MyPlayer"
+            : base(pos, "LeBrink", maxTimePerMove) // choose a string other than "MyPlayer"
         {
             this.position = pos;
             this.maxTime = maxTimePerMove;
-            //this.weightsPackage = weightsPackage;
         }
 
+        /* getImage
+         * this returns an image of me
+         */
         public override string getImage()
         {
             return "lebrink.png";
         }
 
-        public bool isBetterMove(Position currentPosition, int bestValue, int currentValue)
+        /* gloat
+         * This took me like 10 minutes to make. Please help me.
+         */
+        public override string gloat()
         {
+            return "░░░░░░░░░░░░░░░░░░░▒▓▓█████████████▓▓▒░░░░░░░░░░░░░░░░░░░░░░\n" +
+                   "░░░░░░░░░░░░░░▒████▓▓▒▒░▒▒▒░▒▒▒▒▒▒▓▓████▓▒░░░░░░░░░░░░░░░░░░\n" +
+                   "░░░░░░░░░░░▒███▓░░░░░░░░░░░░░░░░░░░░░░▒███████▓▓▒░░░░░░░░░░░\n" +
+                   "░░░░░░░░░▒██▓░░░░▒▒███▓▓▒░░░░░░░░░░░░░▓▓▒▒▒▒▒▓██████▓░░░░░░░\n" +
+                   "░░░░░░░▒██▓░░░▓███▓▒░░░░░░░░▒▒▒▒▒▒▒▒▒▒░░░░░░▒▓▓███▓████▒▒░░░\n" +
+                   "░░░░░░██▓░░▒▓██▓░░▒▓██████▓░░░░░░░▒░░░░░░▒██▓▒░░░▓███▒▓▒░░░░\n" +
+                   "░░░░░██░░▓███▒░░▒██▒░░░░▒▒██▓░░░░░░░░░░░██▒░░░░▒████▒█░░░░░░\n" +
+                   "░░░░██░▒▓▒▓▓░░░██░░░░░░░░░░░█▓░░░░░░░░░██░░░░░░▒███░░█▒░░░░░\n" +
+                   "░░░▓█░░░░░░░░░██░░░░░░░░░░░░▓█░░░░░░░░██░░░░░░░░░░░░░█▒░░░░░\n" +
+                   "░░░█▓░░░░░░░░░█▒░░████░░░░░░░█▒░░░░░░░██░░░░░░░░░░░░███░░░░░\n" +
+                   "░░▒█░░░░░░░▒▓▒█▓░▓████▓░░░░░▒█░░░░░░░░▒█▒░░░░░░░░░░██░█▒░░░░\n" +
+                   "░░██░░░░░▒▓▒▓▒██▒▒▓▓▓░░░░░░░██░░░░░░░░░▒████▓███████▓░█▒░░░░\n" +
+                   "░░█▓░░░░░▒░░░▒░▒██▓▒░░░░░▒██▓░░░░░░░░░░░░░░██▓░░░░░░▒██▓░░░░\n" +
+                   "░░█░░░░░░░░░▓▒░░░░▒▓██████▓░░░░░░░░░░░░░░▒██░░░▓█▓▓▒░░░██░░░\n" +
+                   "░▒█░░░░░░░░░░░░░░░░░░░░░░░░░░▓▒▓▒▒▒▒▒▓▓▓▓██░░▓█▓░▒▒██▒░░██░░\n" +
+                   "░▓█░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▓▓▒░░██░░██▓░▒░▒░██░░▒█░░\n" +
+                   "░██░░░░░░░░░░░░░░░░░░░░░░░▒▓▒▒▒▒▒▒▒▒░░░██░░▓█░█▓░█▒█▓█▓░░█░░\n" +
+                   "░██░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒░▒▒░░▓█▓░░██░█▒▒█▒█▒▓█░░█░░\n" +
+                   "░██░░░░░░░░░░░░░░░░░░░░░░░░▒░░░░░░░░░░▓█░░░█▒░░░░▒▒░░▒█░▓█░░\n" +
+                   "░▒█░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒░░█▒░░█▒░░░░░░░░▓█░█▓░░\n" +
+                   "░░█▓░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓█░░█▒░░░░░░░░█░▒█░░░\n" +
+                   "░░▓█░░▒░░▒▒░░▒░░░░░░░░░░░░░░░░░░░░░░░░░░█░░█▒░░░░░░░█▓░█▓░░░\n" +
+                   "░░░█▒░░▒░░▒░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░█░░█▒░░░░░░▓█░░█░░░░\n" +
+                   "░░░██░░░▒░▒░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░█░░█▒░░░░░░██░░█░░░░\n" +
+                   "░░░░█▓░░░▒░▒░░░░▒▒░░░░░▒▒▒▒▒▒░░░░░░░░░░▒█░▒█░░░░░░░█▒░░█▓░░░\n" +
+                   "░░░░▓█░░░░▒▒░░░░░▒▒░░░░░░▒▒▒▓▓▓▒░░░░░░░██░██░░░░░░░██░░▓█░░░\n" +
+                   "░░░░░██░░░▒░▒░░░░░▒░░░░░░░░▒░▒▒▓█▒░░░░▒█░░█▓▒▓▓▓▒░░▓█░░░█▒░░\n" +
+                   "░░░░░▒█▒░░░▒▒▒░░░░▒░░░░░░░░░░▒▒▒░▒▓░░░██░▒█░░░░▒▓▓░░██░░█▒░░\n" +
+                   "░░░░░░▒█▒░░░▒░▒░░░▒░░░░░░▒▒▒░░░░▒▒░░░▒█░░██░░░░░░░█░▒█░░█▒░░\n" +
+                   "░░░░░░░▓█░░░▒░▒░░░░▒▒░░░░▓▒▒▓▓▓▒░░▓▒░██░░██▒▒▒▒▓▒▓▓███░░█▒░░\n" +
+                   "░░░░░░░░██░░░▒░▒░░░░░▒▒░░░░░░░░▓█▓░░░█▓░░██░▓█░█░█░░█▒░░█▒░░\n" +
+                   "░░░░░░░░░██░░░░▒▒░░░░░░▒▒░░░░░░░░▒█▓░█▓░░▓█▒▒█▒█░█▒██░░▒█░░░\n" +
+                   "░░░░░░░░░░██▒░░░░▒░░░▒░░░▒▒░░░░░░░░▒▓██░░░██░░░░▒▒██░░░██░░░\n" +
+                   "░░░░░░░░░░░▓██░░░░░░░░▒▒░░░▒░░░░░░░░░▓█░░░░▓███▓▓██░░░██░░░░\n" +
+                   "░░░░░░░░░░░░░▓██▒░░░░░░▒▒▒▒▒░░░░░░░░░░██░░░░░░▒▒▒░░░░██░░░░░\n" +
+                   "░░░░░░░░░░░░░░░▓███▒░░░░░░░▒▒▒▒▒▒▒▒░░░░▓██▒░░░░░░░▒███░░░░░░\n" +
+                   "░░░░░░░░░░░░░░░░░▒▓███▓▒░░░░░░░▒░░▒▒▒▒░░░▒██▓██████▓░░░░░░░░\n" +
+                   "░░░░░░░░░░░░░░░░░░░░░▒████▓▒▒░░░░░░░░░░░░░░░▓██▒░░░░░░░░░░░░\n" +
+                   "░░░░░░░░░░░░░░░░░░░░░░░░░▒▓████▓░░░░░░░▓█████▒░░░░░░░░░░░░░░\n" +
+                   "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒█████████▒░░░░░░░░░░░░░░░░░░░\n" +
+                   "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░░░░░\n";
+
+
+        }
+
+        /* isBetterMove
+         * desc: returns true if minValue or maxValue should equal currentValue (does min when needed and max when needed)
+         */
+        public bool isBetterMove(Position currentPosition, int bestValue, int currentValue, int depth)
+        {
+            //if bestValue is minValue, which is default, set it to currentValue
             if (bestValue == int.MinValue)
                 return true;
 
+            //if it is my turn, maximize value by returning true if it is greater than that, else do minimize
             if (currentPosition == this.position)
                 return currentValue > bestValue;
             else
                 return bestValue > currentValue;
         }
 
+        /* minimaxVal with Alpha-Beta Pruning
+         * returns evaluation if it reaches maxDepth, else calculates min or max if it is opponent's turn or my turn respectively
+         * 
+         */
         public DataWrapper minimaxVal(Board b, int d, int alpha, int beta)
         {
-
+            //when time runs out, this evaluates to true
+            //when the cancelled execption is thrown, the last best move will be returned in the chooseMove() try->catch statement
             if (cancellationToken.IsCancellationRequested)
                 throw new OperationCanceledException();
 
+            //return if reached gameover or max depth
             if (b.gameOver() || d == 0)
-                return new DataWrapper(0, evaluate(b));
+                return new DataWrapper(0, evaluate(b), 0 , 0);
 
             DataWrapper data;
 
+            //look through right positions for moves
             int start = 0;
             int end = 5;
 
@@ -109,44 +179,62 @@ namespace Mankalah
                 end = 12;
             }
 
-            int bestValue = int.MinValue;
+            //initialize variables
             int bestMove = -1;
+            int secondBestMove = -1;
 
+            int bestValue = int.MinValue;
+            int secondBestValue = int.MinValue;
+
+            //loop through all possible moves
             for (int move = start; move <= end; move++)
             {
+                //if it is a legal move, check the value of it
                 if (b.legalMove(move))
                 {
-                    
+                    //create copy of board, make move, and evaluate it by recursing
                     Board newBoard = new Board(b);
-                    newBoard.makeMove(move, false);//test
+                    newBoard.makeMove(move, false);
                     data = minimaxVal(newBoard, d - 1, alpha, beta);
                     
-                    if (b.whoseMove() == this.position)
+                    //if value, move pair is best or worst, set bestValue and bestMove to it
+                    //also set second best value, move pair to previous value
+                    if (isBetterMove(b.whoseMove(), bestValue, data.getValue(), d))
                     {
-                        alpha = Math.Max(alpha, data.getValue());
-                    }
-                    else
-                    {
-                        beta = Math.Min(beta, data.getValue());
-                    }
-
-                   
-
-                    if (isBetterMove(b.whoseMove(), bestValue, data.getValue()))
-                    {
+                        secondBestValue = bestValue;
                         bestValue = data.getValue();
+                        
+                        secondBestMove = bestMove;
                         bestMove = move;
+
+                        //alpha beta pruning
+                        if (b.whoseMove() == this.position)
+                        {
+                            alpha = Math.Max(alpha, bestValue);
+                        }
+                        else
+                        {
+                            beta = Math.Min(beta, bestValue);
+                        }
+
                     }
+                    
+                    //if alpha is greater than or equal to beta, we can skip other moves (pruning part of alpha beta pruning)
                     if (alpha >= beta)
                         break;
                 }
             }
 
-            return new DataWrapper(bestMove, bestValue);
+            //return move pairs
+            return new DataWrapper(bestMove, bestValue, secondBestMove, secondBestValue);
 
         }
 
-        public double pointsOnMyTop(Board b)
+        /* (unused) pointsOnMyTop
+         * gives high score for having number of marbles to be evenly distributed on your side
+         * gives estimate for top (reason this is top is because it makes the evalute function easy to make since it just does a negative value of this if i am bottom)
+         */
+        public int pointsOnMyTop(Board b)
         {
             int bottomPoints = 0;
             int bottomMin = int.MaxValue;
@@ -175,7 +263,11 @@ namespace Mankalah
             return (topMin - topMax)- (bottomMin - bottomMax);
         }
 
-        public double capturesForTop(Board b)
+        /* capturesForTop
+         * estimates how many captures can be made in future board states from current game state
+         * gives estimate for favoring top
+         */
+        public int capturesForTop(Board b)
         {
             int bottomCaptures = 0;
             for (int pos = 0; pos <= 5; pos++)
@@ -209,7 +301,11 @@ namespace Mankalah
             return (topCaptures - bottomCaptures);
         }
 
-        public double moveAgainsTop(Board b)
+        /* moveAgainsTop
+         * estimates how many move agains can be made in the next move from current board game state
+         * gives estimate for favoring top
+         */
+        public int moveAgainsTop(Board b)
         {
             int top = 0;
             for (int pos = 12; pos >= 7; pos--)
@@ -233,53 +329,51 @@ namespace Mankalah
 
         }
 
-        public double topCala(Board b)
-        {
-            return (b.stonesAt(topGoal) - b.stonesAt(bottomGoal));
-        }
-
-        public int calculationTopValue(Board b)
-        {
-            int evaluation = 0;
-            evaluation += (int) (200 * topCala(b));// + (int) (50 * capturesForTop(b));//
-            /*if (turnCount > 10000)
-                evaluation += (int) (50 * pointsOnMyTop(b));*/// + (b.stonesAt(topGoal) + b.stonesAt(bottomGoal))*pointsOnMyTop(b)+40*capturesForTop(b));
-            return evaluation;
-            //weightsPackage.weight1 * topCala(b) + weightsPackage.weight2 * moveAgainsTop(b) +
-            // weightsPackage.weight3 * capturesForTop(b)));// + weightsPackage.weight4 * pointsOnMyTop(b)));
-        }
-
+        /* evaluate
+         * returns score of my player
+         */
         public override int evaluate(Board b)
         {
             if (this.position == Position.Top)
             {
-                return calculationTopValue(b);
+                return b.stonesAt(topGoal) - b.stonesAt(bottomGoal) + (pointsOnMyTop(b)+moveAgainsTop(b)) / depthMax + capturesForTop(b);
             }
             else
             {
-                return -1 * calculationTopValue(b);
+                return b.stonesAt(bottomGoal) - b.stonesAt(topGoal) - (pointsOnMyTop(b) + moveAgainsTop(b)) / depthMax - capturesForTop(b);
             }
 
         }
 
-        public override int chooseMove(Board b) { 
+        /* chooseMove
+         * Description: Goes through every depth starting at 1, until time runs out at which point it will return last bestMove
+         */
+        public override int chooseMove(Board b)
+        {
+
             cancellationToken = new CancellationTokenSource(TimeSpan.FromMilliseconds(maxTime));
-            var depth = 1;
-            int bestMove = minimaxVal(b, depth, int.MinValue, int.MaxValue).getMove();
+            depthMax = 1;
+            DataWrapper data;
+            data = minimaxVal(b, depthMax, int.MinValue, int.MaxValue);
+
             turnCount++;
-            depth++;
+            depthMax++;
+
+
+
             try
             {
                 while (true)
                 {
-                    bestMove = minimaxVal(b, depth, int.MinValue, int.MaxValue).getMove();
-                    depth++;
+                    data = minimaxVal(b, depthMax, int.MinValue, int.MaxValue);
+                    depthMax++;
                 }
             }
             catch (OperationCanceledException e)
             {
-                Console.WriteLine(depth);
-                return bestMove;
+                if (data.getSecondBestValue() - data.getValue() == 1 && turnCount % 9 == 0)
+                    return data.getSecondBestMove();
+                return data.getMove();
             }
 
         }
